966cf6aa4bdc9169d3e457cd31c7d53171b2c691
Christian Barrow <Christian.Barrow@walmart.com>
2017-08-29 08:32:41 -0500

    added methods to player-detail comp

--- src/app/player-detail/player-detail.component.ts
+++ src/app/player-detail/player-detail.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Player } from '../model';
+import { PlayerService } from '../player.service';
+
+import 'rxjs/add/operator/switchMap';
+
 @Component({
   selector: 'app-player-detail',
   templateUrl: './player-detail.component.html',
   styleUrls: ['./player-detail.component.scss']
 })
 export class PlayerDetailComponent implements OnInit {
+  player: Player;
 
-  constructor() { }
+  constructor(
+    private playerService: PlayerService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.route.paramMap
+      .switchMap((params: ParamMap) => this.playerService.getPlayer(+params.get('id')))
+      .subscribe(player => this.player = player);
   }
 
+  save(): void {
+    this.playerService.update(this.player)
+      .subscribe(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
