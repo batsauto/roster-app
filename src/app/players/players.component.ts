d4d73c09b7925ee7c8f0f46ce4acd6b53ff3bbd0
Christian Barrow <Christian.Barrow@walmart.com>
2017-08-29 08:23:53 -0500

    added methods to player comp

--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Player } from '../model';
+
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-players',
@@ -7,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersComponent implements OnInit {
 
-  constructor() { }
+  players: Player[];
+  selectedPlayer: Player;
 
-  ngOnInit() {
+  constructor(
+    private playerService: PlayerService,
+    private router: Router) { }
+
+  getPlayers(): void {
+    this.playerService
+        .getPlayers()
+        .subscribe(players => this.players = players);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.playerService.create(name)
+      .subscribe(player => {
+        this.players.push(player);
+        this.selectedPlayer = null;
+      });
+  }
+
+  delete(player: Player): void {
+    this.playerService
+        .delete(player.id)
+        .subscribe(() => {
+          this.players = this.players.filter(p => p !== player);
+          if (this.selectedPlayer === player) { this.selectedPlayer = null; }
+        });
+  }
+
+
+  ngOnInit() {
+    this.getPlayers();
+  }
+
+  onSelect(player: Player): void {
+    this.selectedPlayer = player;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['detail', this.selectedPlayer.id]);
+  }
 }
